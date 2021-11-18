2892613e475678de9a5129d2ef135b27ea9bdba1
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-18 17:24:04 +0100

    :sparkles: Add server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((response) => response.json())
+      .then((result) => setMessage(result.message));
+  }, []);
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>{message}</h1>
     </>
   );
 }
